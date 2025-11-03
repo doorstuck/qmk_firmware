b78910c8b85f02b143526b0f4bd2e906394a35a9
Andrey <doorstuck1@gmail.com>
2025-11-02 21:39:30 -0800

    update to correct map

--- keyboards/boardsource/lulu/keymaps/doorstuck/keymap.c
+++ keyboards/boardsource/lulu/keymaps/doorstuck/keymap.c
@@ -45,18 +45,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------.    ,-------| Left | Down |  Up  |Right |      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|   +  |   =  |   _  |   -  |      |      |
+ * |      |      |      |      |      |      |-------|    |-------|   +  |   =  |   _  |   -  |  \   |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /       /       \      \  |      |      |      |
+ *                   |      |      |      | /       /       \      \  |      |      | DEL  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC,  KC_RBRC, XXXXXXX,                XXXXXXX, KC_LPRN, KC_RPRN, KC_LCBR,  KC_RCBR, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_PLUS, KC_EQL,  KC_UNDS, KC_MINS, XXXXXXX, XXXXXXX,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,                KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT, KC_RGHT, XXXXXXX,
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_PLUS, KC_EQL,  KC_UNDS, KC_MINS,KC_BSLS, XXXXXXX,
+                             _______, _______, _______, _______, _______,  _______, _______, KC_DEL 
 ),
 [_RAISE] = LAYOUT(
   RM_NEXT, RM_PREV, RM_SATU, RM_SATD, RM_VALU, RM_VALD,                     _______, _______, _______, _______, _______, _______,
